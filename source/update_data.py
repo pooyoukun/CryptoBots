7817363738f30152694d0b484295effc91c38f8a
pooyoukun <67104482+pooyoukun@users.noreply.github.com>
2022-04-07 21:21:19 +0200

    moved folders

---
+++ source/update_data.py
@@ -0,0 +1,25 @@
+from ast import Break
+import pandas as pd
+import ccxt
+import json
+import os
+
+from data_engine import DataEngine
+
+print(os.getcwd())
+#Break
+
+# -- Data variable --
+f = open('./data/pair_list.json',)
+pair_list = json.load(f)
+f.close()
+
+f = open('./data/timeframe_list.json',)
+timeframe_list = json.load(f)
+f.close()
+
+# -- Instance class --
+dataEngine = DataEngine(session=ccxt.binance())
+
+# -- Download data from data variable --
+dataEngine.update_data(pair_list, timeframe_list)
